aec7b978733c3f5318ba0dd8e4f63397cb6efd56
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2025-06-16 10:31:17 -0400

    Initial commit of minimal Vite React template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import './styles/styles.css';
+import TempComponent from './components/TempComponent/TempComponent';
+
+function App() {
+  return (
+    <>
+      <TempComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TempComponent/TempComponent.jsx
@@ -0,0 +1,3 @@
+export default function TempComponent() {
+  return <h1>Placeholder Text</h1>;
+}
